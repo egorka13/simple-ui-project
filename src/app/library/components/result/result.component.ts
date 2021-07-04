2708c874ee2006933af661d2c7a05a40b5b3f9ae
W014ara <bezruk2000@mail.ru>
2021-07-04 17:46:56 +0300

    MSK21QAS-7[library]-fixing-the-styles-of-the-component-when-passing-a-non-standard-path-to-the-icon

--- src/app/library/components/result/result.component.ts
+++ src/app/library/components/result/result.component.ts
@@ -9,6 +9,8 @@ import { resultTypes, TypeResults } from './resultTypes';
 export class ResultComponent implements OnInit {
     types: TypeResults = resultTypes;
     generalType: string = '';
+    generalCurType: string = '';
+    generalCurAlt: string = '';
 
     @Input()
     title: string = "Here's sample title";
@@ -29,5 +31,18 @@ export class ResultComponent implements OnInit {
 
     ngOnInit(): void {
         this.generalType = this.types[this.type] || this.type;
+        this.generalCurType = this.checkValidType(this.types[this.type]) ? this.type : '';
+        this.generalCurAlt = this.checkValidType(this.types[this.type])
+            ? `here ${this.type} image`
+            : `${this.title} image`;
+    }
+
+    /**
+     * method of checking for a non-standard input path to the picture.
+     * @param data original input's data
+     * @returns returns false if the value is undefined, true otherwise
+     */
+    checkValidType(data: string): boolean {
+        return data !== undefined ? true : false;
     }
 }
