e8d738950a99c246226dd2e88aa96939be3a2e41
Alexey Pomazan <a.pomazan@didlogic.com>
2021-07-05 04:03:01 +0300

    MSK21QAS-10 [library] added card component

--- src/app/library/library.module.ts
+++ src/app/library/library.module.ts
@@ -3,20 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-    declarations: [
-        ButtonComponent
-    ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        CommonModule
-    ],
-    exports: [
-        ButtonComponent
-    ],
-    providers: []
+    declarations: [ButtonComponent, CardComponent],
+    imports: [BrowserModule, FormsModule, CommonModule],
+    exports: [ButtonComponent, CardComponent],
+    providers: [],
 })
-export class LibraryModule {
-}
+export class LibraryModule {}
