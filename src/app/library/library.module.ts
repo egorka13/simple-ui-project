27634262080b241fe4c84f37a39e5edd6b9f1bc6
W014ara <bezruk2000@mail.ru>
2021-07-05 19:06:16 +0300

    MSK21QAS-7[library] fixed-result-component-name

--- src/app/library/library.module.ts
+++ src/app/library/library.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
 import { ResultComponent } from './components/sui-result/result.component';
+import { SliderComponent } from './components/sui-slider/slider.component';
 
 @NgModule({
     declarations: [
         ButtonComponent,
         ResultComponent,
+        SliderComponent,
     ],
     imports: [
         BrowserModule,
@@ -18,6 +20,7 @@ import { ResultComponent } from './components/sui-result/result.component';
     exports: [
         ButtonComponent,
         ResultComponent,
+        SliderComponent
     ],
     providers: []
 })
