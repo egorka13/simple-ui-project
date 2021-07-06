3e3e843747bce79ac3baa6cdc960ea8493996e83
W014ara <bezruk2000@mail.ru>
2021-07-06 14:54:24 +0300

    MSK21QAS-7[library] fixed component style name

--- src/app/library/components/sui-result/resultTypes.ts
+++ src/app/library/components/sui-result/resultTypes.ts
@@ -1,3 +1,6 @@
+//Rename this if you need a different path to the standard library icons
+const path: string = 'app/library/assets/icons/result/';
+
 export interface TypeResults {
     [key: string]: string;
     success: string;
@@ -9,15 +12,12 @@ export interface TypeResults {
     error: string;
 }
 
-//Rename this if you need a different path to the standard library icons
-const path: string = 'app/library/assets/icons/result/';
-
 export const resultTypes: TypeResults = {
     success: path + 'success.svg',
     info: path + 'info.svg',
     warning: path + 'warning.svg',
     403: path + '403.svg',
     404: path + '404.svg',
-    500: 'app/library/assets/icons/result/500.svg',
+    500: path + '500.svg',
     error: path + 'error.svg',
 };
