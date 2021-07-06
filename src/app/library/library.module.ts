00bfac5a9aec0abd888290c70a06cae022dc5eef
W014ara <bezruk2000@mail.ru>
2021-07-06 17:57:34 +0300

    MSK21QAS-7[library] removed-slider-component

--- src/app/library/library.module.ts
+++ src/app/library/library.module.ts
@@ -4,13 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
 import { ResultComponent } from './components/sui-result/result.component';
-import { SliderComponent } from './components/sui-slider/slider.component';
 
 @NgModule({
     declarations: [
         ButtonComponent,
         ResultComponent,
-        SliderComponent
     ],
     imports: [
         BrowserModule,
@@ -20,7 +18,6 @@ import { SliderComponent } from './components/sui-slider/slider.component';
     exports: [
         ButtonComponent,
         ResultComponent,
-        SliderComponent
     ],
     providers: []
 })
