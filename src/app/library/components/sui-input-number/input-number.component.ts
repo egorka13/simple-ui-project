042c2dfd808a360734b339029dd58e3cab72da55
Alexey Pomazan <a.pomazan@didlogic.com>
2021-07-07 21:39:30 +0300

    edited number input

---
+++ src/app/library/components/sui-input-number/input-number.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit, EventEmitter, Input, Output, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+    selector: 'app-input-number',
+    templateUrl: './input-number.component.html',
+    styleUrls: ['./input-number.component.less'],
+})
+export class InputNumberComponent implements OnInit {
+    @Output() valueChange = new EventEmitter();
+    @Input() value: any = null;
+    @Input() min: number = 1;
+    @Input() max: number = 99;
+
+    @ViewChild('inputNumber')
+    inputElement: any;
+
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    change(): void {
+        if (this.value) {
+            if (isNaN(this.inputElement.nativeElement.value)) {
+                this.value = 1;
+                console.log(typeof this.inputElement.nativeElement.value);
+
+                this.valueChange.emit(this.value);
+                return;
+            }
+        }
+        if (this.max) {
+            if (this.value > this.max) {
+                this.value = this.max;
+                this.valueChange.emit(this.value);
+                return;
+            }
+        }
+        if (this.min) {
+            if (this.value < this.min && this.value !== '') {
+                this.value = this.min;
+                this.valueChange.emit(this.value);
+                return;
+            }
+        }
+        this.value = this.inputElement.nativeElement.value;
+        this.valueChange.emit(this.value);
+    }
+
+    onIncrement(): void {
+        ++this.value;
+        if (this.max) {
+            if (this.value > this.max) {
+                this.value = this.max;
+                this.valueChange.emit(this.value);
+                return;
+            }
+        }
+        this.valueChange.emit(this.value);
+    }
+
+    onDecrement(): void {
+        --this.value;
+        if (this.min) {
+            if (this.value < this.min) {
+                this.value = this.min;
+                this.valueChange.emit(this.value);
+                return;
+            }
+        }
+        this.valueChange.emit(this.value);
+    }
+}
--- src/app/library/library.module.ts
+++ src/app/library/library.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
-import { InputNumberComponent } from './input-number/input-number.component';
+import { InputNumberComponent } from './components/sui-input-number/input-number.component';
 
 @NgModule({
     declarations: [ButtonComponent, InputNumberComponent],
