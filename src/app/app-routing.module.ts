a4a1c6998b315967ce5a2edfb3a17c403e092012
Alexander Timofeev <Timofeev.Alex.W@yandex.ru>
2021-07-11 20:34:53 +0000

    MSK21QAS-12 [shared] fixed typo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { WrapperComponent } from './components/wrapper/wrapper.component';
 
 // TODO: Done for demo testing right now. Feel free to change paths.
 // If you need a space for testing your component you can even add a separate path to it.
-// For exampe: { path: 'b', component: BoardComponent }
+// For example: { path: 'b', component: BoardComponent }
 const routes: Routes = [
     { path: '', component: WrapperComponent },
     { path: 'lib', component: LibraryShowcaseComponent },
