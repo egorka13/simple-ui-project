0b234d55ff9903817696206c7ed690594dd571ae
W014ara <bezruk2000@mail.ru>
2021-07-16 00:55:53 +0300

    MSK21QAS-13[header] created-base-layout-and-added-svg-plugin

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BoardSettingsService } from 'src/app/core/services/board-settings.service';
 
 @Component({
     selector: 'sui-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.less'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+
+    public scale:number;
+
+    constructor(public boardSettingsService: BoardSettingsService){
+    
+    }
+
+    ngOnInit():void{
+        this.scale = this.boardSettingsService.scale * 100;
+    }
+}
