a4347c2e8d73a01b68f4484c0f59b52e185fd8a8
W014ara <bezruk2000@mail.ru>
2021-07-16 00:55:53 +0300

    MSK21QAS-13[header] created-base-layout-and-added-svg-plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,16 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { LibraryModule } from './library/library.module';
-import { HeaderComponent } from '@components/header/header.component';
-import { ConfigPanelComponent } from '@components/config-panel/config-panel.component';
-import { ComponentPanelComponent } from '@components/component-panel/component-panel.component';
-import { BoardComponent } from '@components/board/board.component';
-import { BoardItemComponent } from '@components/board/board-item/board-item.component';
-import { WrapperComponent } from '@components/wrapper/wrapper.component';
-import { LibraryShowcaseComponent } from '@components/library-showcase/library-showcase.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ConfigPanelComponent } from './components/config-panel/config-panel.component';
+import { ComponentPanelComponent } from './components/component-panel/component-panel.component';
+import { BoardComponent } from './components/board/board.component';
+import { BoardItemComponent } from './components/board/board-item/board-item.component';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
+import { LibraryShowcaseComponent } from './components/library-showcase/library-showcase.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @NgModule({
     declarations: [
@@ -25,7 +28,7 @@ import { LibraryShowcaseComponent } from '@components/library-showcase/library-s
         ComponentPanelComponent,
         LibraryShowcaseComponent,
     ],
-    imports: [LibraryModule, BrowserModule, AppRoutingModule, FormsModule],
+    imports: [HttpClientModule, AngularSvgIconModule.forRoot(), LibraryModule, BrowserModule, AppRoutingModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
