767964bdd4cb92e3b253be1203d6c223dbaad37c
Alexander Timofeev <Timofeev.Alex.W@yandex.ru>
2021-07-16 08:09:23 +0000

    MSK21QAS-15 [board] and another missing return types

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -29,7 +29,7 @@ export class BoardComponent implements AfterViewInit, OnDestroy {
         this.setZoomListener();
     }
 
-    ngOnDestroy() {
+    ngOnDestroy(): void {
         this.toUnsubscribe.forEach(sub => {
             sub.unsubscribe();
         });
@@ -41,7 +41,7 @@ export class BoardComponent implements AfterViewInit, OnDestroy {
      * @private
      * @memberof BoardComponent
      */
-    private setZoomListener() {
+    private setZoomListener(): void {
         const wheel$: Observable<WheelEvent> = fromEvent<WheelEvent>(document, 'wheel', { passive: false }).pipe(
             filter(e => e.ctrlKey),
             map(e => {
