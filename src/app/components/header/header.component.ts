fe9be78f12f9ffdb2c7a236d29dbf1c5aa6bd175
W014ara <bezruk2000@mail.ru>
2021-07-16 22:20:59 +0300

    MSK21QAS-13[header] added-zoom-logic

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,15 +6,59 @@ import { BoardSettingsService } from 'src/app/core/services/board-settings.servi
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.less'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit{
 
-    public scale:number;
+    public gridState:boolean = false;
 
-    constructor(public boardSettingsService: BoardSettingsService){
-    
+    public getGridPath():string{
+        return !this.gridState ? "../../../assets/icons/header-grid-btn.svg" : "../../../assets/icons/header-grid-none.svg";
     }
 
+
+    public onClickSetGridState(){
+        this.gridState = !this.gridState;
+    }
+
+    public getRounderScale():number{
+        return Math.round(this.boardSettingsService.scale * 100);
+    }
+
+    constructor(public boardSettingsService: BoardSettingsService){}
     ngOnInit():void{
-        this.scale = this.boardSettingsService.scale * 100;
+    }
+
+
+    private checkValidValue(value:number):number{
+        if(value > 2){
+            return 2;
+        }
+        if(value < .3){
+            return .3;
+        }
+        return value;
+    }
+
+    onChangeInput(event: Event):void{
+        const targetInput: HTMLInputElement = <HTMLInputElement>event.target;
+        let currentValue:number = Number(targetInput.value) / 100;
+        this.boardSettingsService.setScale(this.checkValidValue(currentValue));
+    }
+
+    onClickInput(event: Event):void{
+        const targetInput: HTMLInputElement = <HTMLInputElement>event.target;
+        targetInput.value = "";
+    }
+
+    onKeyUpInput(event: Event):void{
+        const targetInput: HTMLInputElement = <HTMLInputElement>event.target;
+        targetInput.value = targetInput.value.replace(/[a-zа-яё]/gi, '');
+    }
+
+    onClickZoomIn():void{
+        this.boardSettingsService.changeScale(1);
+    }
+
+    onClickZoomOut():void{
+        this.boardSettingsService.changeScale(-1);
     }
 }
