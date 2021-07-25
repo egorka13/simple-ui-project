46d4a67803d55a1af886dc97d7d12aca3b02acd6
Irina Tarasova <izotarina@yandex.ru>
2021-07-26 00:14:19 +0300

    MSK21QAS-16 [library] changed form of inputs in radio, button and slider components

--- src/app/library/components/radio/radio.component.ts
+++ src/app/library/components/radio/radio.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { RadioItem } from './radio.model';
+import { RadioItem, radioType } from './radio.model';
 
 @Component({
     selector: 'sui-radio',
@@ -11,23 +11,22 @@ export class RadioComponent implements OnInit {
     typeModifier: string = '';
 
     @Input()
-    name: string = 'radio' + Math.random().toFixed(5);
+    name: string = 'radio' + Date.now().toFixed(6);
 
     @Input()
-    labels: string = 'Radio';
+    label: string[] = ['Radio'];
 
     @Input()
-    type: undefined | 'button';
+    set type(value: radioType) {
+        this.typeModifier =  value ? '_type_' + value : '';
+    }
 
     ngOnInit(): void {
-        const labelsArr: string[] = this.labels.split(' ');
-        for (let i: number = 0; i < labelsArr.length; i++) {
+        for (let i: number = 0; i < this.label.length; i++) {
             this.items.push({
                 id: [this.name, i].join('_'),
-                text: labelsArr[i],
+                text: this.label[i],
             });
         }
-
-        this.typeModifier = this.type ? '_type_button' : '';
     }
 }
