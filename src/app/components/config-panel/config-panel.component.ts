66b891d982ee9257d2e479e3da0b24d00e9105ed
Irina Tarasova <izotarina@yandex.ru>
2021-07-26 15:18:58 +0300

    MSK21QAS-16 [conf_panel] fixed esLint problems

--- src/app/components/config-panel/config-panel.component.ts
+++ src/app/components/config-panel/config-panel.component.ts
@@ -19,7 +19,7 @@ export class ConfigPanelComponent implements OnInit {
 
     constructor(private ConfigDataService: ConfigDataService) { }
 
-    ngOnInit() {
+    ngOnInit(): void {
         this.ConfigDataService.setConfigData$.subscribe((config: ILibComponentConfig | null) => {
             if (!config) return;
 
@@ -32,10 +32,9 @@ export class ConfigPanelComponent implements OnInit {
     }
 
     public selectTab(event: Event): void {
-        const element: Element = <Element>event.target;
+        const element: Element = event.target as Element;
         this.isDesign = element.id == 'designSection';
-        
-        const parent: Element = <Element>element.parentElement;
+        const parent: Element = element.parentElement as Element;
         [].forEach.call(parent.children, (el: Element) => {
             el.classList.remove('_active');
         });
@@ -43,9 +42,8 @@ export class ConfigPanelComponent implements OnInit {
     }
 
     public handlePropertyChange(event: Event): void {
-        const propertyName: string = (<HTMLTextAreaElement>event.target).name; 
+        const propertyName: string = (event.target as HTMLTextAreaElement).name; 
         const [parseNameArr, parseName]: [string[], string] = this.parsePropertyName(propertyName);
-        
         if (this.properties[propertyName]) {
             this.properties[propertyName].value = this.convertToInitialType(this.properties[propertyName].value)
             this.sendDataChange(propertyName, this.properties[propertyName]);
@@ -56,19 +54,18 @@ export class ConfigPanelComponent implements OnInit {
 
             let numberOfValue: number = +parseNameArr[parseNameArr.length - 1] - 1;
             if (propertyOfParseName.labels) {
-                numberOfValue = propertyOfParseName.labels.indexOf((parseNameArr[parseNameArr.length - 1]));
+                numberOfValue = propertyOfParseName.labels.indexOf(parseNameArr[parseNameArr.length - 1]);
             }
             (propertyOfParseName.value as string[])[numberOfValue] = this.newProperties[parseName].value.toString();
 
             this.sendDataChange(parseName, propertyOfParseName);
-        } 
+        }
     }
 
     private convertToInitialType(value: valueType): valueType {
         if (!isNaN(+value)) {
             return +value;
         }
-        
         switch (value) {
             case 'false':
                 return false;
@@ -83,7 +80,7 @@ export class ConfigPanelComponent implements OnInit {
 
     private sendDataChange(propertyName: string, property: IPropertyObjevtValue): void {
         this.ConfigDataService.saveConfigData({
-            [propertyName]: property
+            [propertyName]: property,
         });
     }
 
@@ -98,18 +95,16 @@ export class ConfigPanelComponent implements OnInit {
         const parsedProperties: IConfigPanelProperty = {};
         const newProperties: IConfigPanelProperty = {};
 
-        for (let [name, prop] of Object.entries(properties)) {
+        for (const [name, prop] of Object.entries(properties)) {
             if (Array.isArray(prop.value)) {
-
                 for (let i: number = 0; i < prop.value.length; i++) {
-                    let newPropName = prop.labels ? [name, prop.labels[i]].join(' ') : [name, i + 1].join(' ');
-                    
-                    newProperties[newPropName] =  {
+                    const newPropName = prop.labels ? [name, prop.labels[i]].join(' ') : [name, i + 1].join(' ');
+                    newProperties[newPropName] = {
                         type: Array.isArray(prop.type) ? prop.type[i] : prop.type,
                         value: prop.value[i] ,
                         isDeleted: prop.isDeleted,
                         isEndOfGroup: prop.isDeleted && i == prop.value.length - 1 
-                    }
+                    };
                 }
 
                 if (prop.labels) {
@@ -120,7 +115,6 @@ export class ConfigPanelComponent implements OnInit {
 
                 parsedProperties[name] = prop;
                 delete properties[name];
-                
             }
         }
 
@@ -128,23 +122,23 @@ export class ConfigPanelComponent implements OnInit {
     }
 
     public deleteProperty(event: Event): void {
-        const elem: Element = <Element>event.target;
+        const elem: Element = event.target as Element;
 
         if (elem.classList.contains('sui-config-panel__property-cross')) {
-            const propertyName: string = (<HTMLTextAreaElement>event.target).dataset.name as string;
+            const propertyName: string = (event.target as HTMLTextAreaElement).dataset.name as string;
             const [parseNameArr, parseName]: [string[], string] = this.parsePropertyName(propertyName);
 
             const property: IPropertyObjevtValue = this.parsedProperties[parseName];
             const propertyValue: string[] = property.value as string[];
-            const valueLength: number = (propertyValue).length;
+            const valueLength: number = propertyValue.length;
             const numberOfProperty: number = +parseNameArr[parseNameArr.length - 1];
 
             let changingName: string = [parseName, numberOfProperty].join(' ');
             for (let i: number = numberOfProperty; i < valueLength; i++) {
-                let nextChangingName: string = [parseName, i + 1].join(' ');
+                const nextChangingName: string = [parseName, i + 1].join(' ');
                 this.newProperties[changingName] = this.newProperties[nextChangingName];
                 changingName = nextChangingName;
-            }        
+            }
             delete this.newProperties[changingName];
             propertyValue.splice(numberOfProperty - 1, 1);
             this.newProperties[[parseName, valueLength - 1].join(' ')].isEndOfGroup = true;
@@ -154,12 +148,12 @@ export class ConfigPanelComponent implements OnInit {
     }
 
     public addProperty(event: Event): void {
-        const propertyName: string = (<HTMLTextAreaElement>event.target).name;
+        const propertyName: string = (event.target as HTMLTextAreaElement).name;
         const [parseNameArr, parseName] = this.parsePropertyName(propertyName);
         const numberOfProperty: number = +parseNameArr[parseNameArr.length - 1];
 
         this.newProperties[propertyName].isEndOfGroup = false;
-        this.newProperties[[parseName, numberOfProperty + 1].join(' ')] =  {
+        this.newProperties[[parseName, numberOfProperty + 1].join(' ')] = {
             type: 'text',
             value: 'text',
             isDeleted: true,
@@ -168,7 +162,7 @@ export class ConfigPanelComponent implements OnInit {
 
         const property: IPropertyObjevtValue = this.parsedProperties[parseName];
         const propertyValue: string[] = property.value as string[];
-        (propertyValue).push('text');
+        propertyValue.push('text');
 
         this.sendDataChange(parseName, property);
     }
--- src/app/models/config-panel.model.ts
+++ src/app/models/config-panel.model.ts
@@ -7,75 +7,81 @@ export interface ILibComponentConfig {
 }
 
 export interface IPropertyObjevtValue {
-    value: valueType,
-    type?: propertyType | propertyType[],
-    options?: string[],
-    isDeleted?: boolean,
-    isEndOfGroup?: boolean,
-    labels?: string[],
+    value: valueType;
+    type?: propertyType | propertyType[];
+    options?: string[];
+    isDeleted?: boolean;
+    isEndOfGroup?: boolean;
+    labels?: string[];
 }
 
 export interface IConfigPanelProperty {
-    [name: string]: IPropertyObjevtValue
+    [name: string]: IPropertyObjevtValue;
 }
 
 export interface IComponentPrototype {
-    [componentName: string]: IConfigPanelProperty,
+    [componentName: string]: IConfigPanelProperty;
 }
 
 export const componentModels: IComponentPrototype = {
     'sui-button': {
-        type: {type: 'select', options: ['default', 'primary', 'dashed', 'text', 'link'], value: ''},
-        size: {type: 'select', options: ['default', 'small', 'large'], value: ''},
-        isGhost: {type: 'select', options: ['false', 'true'], value: ''},
-        innerText: {type: 'text', value: ''}
+        type: { type: 'select', options: ['default', 'primary', 'dashed', 'text', 'link'], value: '' },
+        size: { type: 'select', options: ['default', 'small', 'large'], value: '' },
+        isGhost: { type: 'select', options: ['false', 'true'], value: '' },
+        innerText: { type: 'text', value: '' },
     },
     'sui-card': {
-        size: {type: 'select', options: ['default', 'small'], value: ''},
-        title: {type: 'text', value: ''},
-        text: {type: 'text', value: ''},
-        contentTempalte: {type: 'text', value: ''}
+        size: { type: 'select', options: ['default', 'small'], value: '' },
+        title: { type: 'text', value: '' },
+        text: { type: 'text', value: '' },
+        contentTempalte: { type: 'text', value: '' },
     },
     'sui-checkbox': {
-        labelText: {type: 'text', value: ''},
-        isDisabled: {type: 'select', options: ['false', 'true'], value: ''},
-        isChecked: {type: 'select', options: ['false', 'true'], value: ''}
+        labelText: { type: 'text', value: '' },
+        isDisabled: { type: 'select', options: ['false', 'true'], value: '' },
+        isChecked: { type: 'select', options: ['false', 'true'], value: '' },
     },
     'sui-input': {
-        placeholder: {type: 'text', value: ''},
-        size: {type: 'select', options: ['default', 'small', 'large'], value: ''}
+        placeholder: { type: 'text', value: '' },
+        size: { type: 'select', options: ['default', 'small', 'large'], value: '' },
     },
     'sui-input-number': {
-        value: {type: 'number', value: ''},
-        min: {type: 'number', value: ''},
-        max: {type: 'number', value: ''},
-        size: {type: 'select', options: ['default', 'small', 'large'], value: ''}
+        value: { type: 'number', value: '' },
+        min: { type: 'number', value: '' },
+        max: { type: 'number', value: '' },
+        size: { type: 'select', options: ['default', 'small', 'large'], value: '' },
     },
     'sui-radio': {
-        label: {type: 'text', isDeleted: true, value: ''},
-        type: {type: 'select', options: ['defualt', 'button'], value: ''}
+        label: { type: 'text', isDeleted: true, value: '' },
+        type: { type: 'select', options: ['defualt', 'button'], value: '' },
     },
     'sui-result': {
-        title: {type: 'text', labels: ['text', 'fontFamily', 'fontColor', 'fontSize'], value: ''},
-        subtitle: {type: 'text', labels: ['text', 'fontFamily', 'fontColor', 'fontSize'], value: ''},
-        type: {type: 'select', options: ['success', 'info', 'warning', '403', '404', '500', 'error'], value: ''},
-        hrefGeneral: {type: ['text', 'text', 'color', 'color', 'color'],
-            labels: ['link', 'text', 'backgroundColor', 'borderColor', 'textColor'], value: ''},
-        hrefAdditional: {type: ['text', 'text', 'color', 'color', 'color'],
-            labels: ['link', 'text', 'backgroundColor', 'borderColor', 'textColor'], value: ''},
+        title: { type: 'text', labels: ['text', 'fontFamily', 'fontColor', 'fontSize'], value: '' },
+        subtitle: { type: 'text', labels: ['text', 'fontFamily', 'fontColor', 'fontSize'], value: '' },
+        type: { type: 'select', options: ['success', 'info', 'warning', '403', '404', '500', 'error'], value: '' },
+        hrefGeneral: {
+            type: ['text', 'text', 'color', 'color', 'color'],
+            labels: ['link', 'text', 'backgroundColor', 'borderColor', 'textColor'],
+            value: '',
+        },
+        hrefAdditional: {
+            type: ['text', 'text', 'color', 'color', 'color'],
+            labels: ['link', 'text', 'backgroundColor', 'borderColor', 'textColor'],
+            value: '',
+        },
     },
     'sui-slider': {
-        disabled: {type: 'select', options: ['false', 'true'], value: ''},
-        showMinimumValue: {type: 'select', options: ['false', 'true'], value: ''},
-        showMaximumValue: {type: 'select', options: ['false', 'true'], value: ''},
-        legend: {type: 'text', value: ''},
-        legendColor: {type: 'color', value: ''},
-        sliderColor: {type: 'color', value: ''},
-        valuesColor: {type: 'color', value: ''},
-        wrapperWidth: {type: 'text', value: ''},
-        maxValue: {type: 'number', value: ''},
-        minValue: {type: 'number', value: ''},
-        currentValue: {type: 'number', value: ''},
-        step: {type: 'number', value: ''},
-    }
-}
+        disabled: { type: 'select', options: ['false', 'true'], value: '' },
+        showMinimumValue: { type: 'select', options: ['false', 'true'], value: '' },
+        showMaximumValue: { type: 'select', options: ['false', 'true'], value: '' },
+        legend: { type: 'text', value: '' },
+        legendColor: { type: 'color', value: '' },
+        sliderColor: { type: 'color', value: '' },
+        valuesColor: { type: 'color', value: '' },
+        wrapperWidth: { type: 'text', value: '' },
+        maxValue: { type: 'number', value: '' },
+        minValue: { type: 'number', value: '' },
+        currentValue: { type: 'number', value: '' },
+        step: { type: 'number', value: '' },
+    },
+};
