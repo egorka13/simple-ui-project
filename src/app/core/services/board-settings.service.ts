07a9ecc84aacad410a903d119b00623bbb9d2467
Alexander Timofeev <Timofeev.Alex.W@yandex.ru>
2021-07-28 16:42:55 +0000

    MSK21QAS-15 [board] fixed scale bug on browser return

--- src/app/core/services/board-settings.service.ts
+++ src/app/core/services/board-settings.service.ts
@@ -36,6 +36,7 @@ export class BoardSettingsService {
     }
 
     public setScale(value: number): void {
+        if (typeof value != 'number' || isNaN(value)) return;
         this.scaleState = value;
         this.updateTransformStyle();
     }
